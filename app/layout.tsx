6ffe95d6b595cfa6c573f6c49eaf599cbe5ea5f8
vamshichi <chvamshi03@example.com>
2024-12-31 12:57:04 +0530

    small edits

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from './components/Footer'
 import MetaPixel from './components/MetaPixel'
 import Navbar from './components/Navbar'
+import Script from 'next/script'
 // import Document from './document'
 import './globals.css'
 import type { Metadata } from 'next'
@@ -17,9 +18,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+       <head>
+        <Script
+          id="fb-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '469919302514972');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+      </head>
       <body>
         {/* <Document /> */}
-        <MetaPixel pixelId="469919302514972" />
+        
         <Navbar />
         {children}
         <Footer />
