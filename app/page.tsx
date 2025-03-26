888a17315de0b6f9035ad8fb972b5a7540db7dcb
Sandeep1816 <sandeepavala2@gmail.com>
2025-03-26 15:00:13 +0530

    management is removed

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
       <PopularExhibitions />
       {/* <KeyCategories /> */}
       <FeaturedSponsors />
-      <ManagementTeam />
+      {/* <ManagementTeam /> */}
       <Testimonials />
      
       {/* <Header /> */}
