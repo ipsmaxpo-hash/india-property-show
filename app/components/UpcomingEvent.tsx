d61c27d1f9d24d48e040be04615261dbb4c30793
Sandeep1816 <avalasandeep02@gmail.com>
2025-08-26 15:34:18 +0530

    conflict

--- app/components/UpcomingEvent.tsx
+++ app/components/UpcomingEvent.tsx
@@ -109,7 +109,7 @@ const EventCard: React.FC<{ event: Event }> = ({ event }) => {
           )}
         />
 
-        {/* Button pinned at bottom ......*/}
+        {/* Button pinned at bottom ...ssss...*/}
         <div className="mt-auto">
           <a
             href="/exhibitorregistration"
